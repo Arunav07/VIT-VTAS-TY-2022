55c775449d33eb06a653f4d24bcc3f736fa37084
Arunav Chandra <39475778+Arunav07@users.noreply.github.com>
2022-12-06 00:26:45 +0530

    file splitting

--- main.py
+++ main.py
@@ -10,3 +10,11 @@ with open("text.txt", "r") as f:
         i+=1
         print("{} : {}".format(line.strip(),int(str( bin(int(binascii.hexlify(line.strip().encode('utf8')), 16)))[2:])))
         binaryFile.write("{} : {}\n".format(line.strip(),int(str( bin(int(binascii.hexlify(line.strip().encode('utf8')), 16)))[2:])))
+i=1
+with open ("binary.txt", "r") as myfile:
+    chunk = myfile.read(16)
+    while chunk:
+        with open("chunkFile_"+(str(i))) as chunk_file:
+            chunk_file.write(chunk)
+        i+=1
+        chunk = myfile.read(16)
