3c1f4e1e4256cb4d5c60b9cef3ee9845124f5198
Arunav Chandra <39475778+Arunav07@users.noreply.github.com>
2022-12-07 16:24:22 +0530

    Splitted file into chunks

--- main.py
+++ main.py
@@ -12,11 +12,14 @@ with open("text.txt", "r") as f:
         # print("{} : {}".format(line.strip(),int(str( bin(int(binascii.hexlify(line.strip().encode('utf8')), 16)))[2:])))
         binaryFile.write("{}\n".format(int(str( bin(int(binascii.hexlify(line.strip().encode('utf8')), 16)))[2:])))
 i=1
+binaryFile.close()
+binaryFile = open("binary.txt", "r")
+
+print("Now splitting binary file")
 with open ("binary.txt", "r") as myfile:
     chunk = myfile.read(8)
-    print(chunk)
-    while chunk:
-        with open("chunkFile_"+(str(i)), "wt+") as chunk_file:
+    while (chunk != ""):
+        with open("Chunks/chunkFile_"+(str(i)), "wt+") as chunk_file:
             chunk_file.write(chunk)
         i+=1
-        chunk = myfile.read(8)
+        chunk = myfile.read(8)
