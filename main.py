5accb9830798e936516c683682443df9adfd8284
Arunav Chandra <39475778+Arunav07@users.noreply.github.com>
2022-12-07 18:03:17 +0530

    converted binary files to MDA5 and SHA1 hashes to store them separately,

--- main.py
+++ main.py
@@ -23,3 +23,25 @@ with open ("binary.txt", "r") as myfile:
             chunk_file.write(chunk)
         i+=1
         chunk = myfile.read(8)
+
+
+# Using Hashing algorithms to find duplicates
+import hashlib
+
+print("Now finding duplicates")
+for j in range(1, i-1):
+    with open("Chunks/chunkFile_"+(str(j)), "r") as f:
+        data = f.read()
+        with open("Hashes/MDA5/hashFile_"+(str(j)), "wt+") as hash_file_MDA5:
+            hash_file_MDA5.write(hashlib.md5(data.encode('utf-8')).hexdigest())
+        with open("Hashes/SHA1/hashFile_"+(str(j)), "wt+") as hash_file_SHA1:
+            hash_file_SHA1.write(hashlib.sha1(data.encode('utf-8')).hexdigest())
+
+
+# Using Dedupe to find duplicates in MDA5 Hashes
+
+
+
+# Using Dedupe to find duplicates in SHA1 Hashes
+
+
