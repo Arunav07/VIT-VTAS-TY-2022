94914795448fc839c12e9ba40b5f7f174acbd301
Arunav Chandra <39475778+Arunav07@users.noreply.github.com>
2022-12-07 16:10:10 +0530

    file changes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import binascii
+import dedupe
 
 file = open("text.txt", "r")
 binaryFile = open("binary.txt", "wt+")
@@ -8,13 +9,14 @@ i=0
 with open("text.txt", "r") as f:
     for line in f:
         i+=1
-        print("{} : {}".format(line.strip(),int(str( bin(int(binascii.hexlify(line.strip().encode('utf8')), 16)))[2:])))
-        binaryFile.write("{} : {}\n".format(line.strip(),int(str( bin(int(binascii.hexlify(line.strip().encode('utf8')), 16)))[2:])))
+        # print("{} : {}".format(line.strip(),int(str( bin(int(binascii.hexlify(line.strip().encode('utf8')), 16)))[2:])))
+        binaryFile.write("{}\n".format(int(str( bin(int(binascii.hexlify(line.strip().encode('utf8')), 16)))[2:])))
 i=1
 with open ("binary.txt", "r") as myfile:
-    chunk = myfile.read(16)
+    chunk = myfile.read(8)
+    print(chunk)
     while chunk:
-        with open("chunkFile_"+(str(i))) as chunk_file:
+        with open("chunkFile_"+(str(i)), "wt+") as chunk_file:
             chunk_file.write(chunk)
         i+=1
-        chunk = myfile.read(16)
+        chunk = myfile.read(8)
