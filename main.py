f0c5e943b14950037f2bf4a7ae108fa3f988e646
Arunav Chandra <39475778+Arunav07@users.noreply.github.com>
2022-12-08 14:31:13 +0530

    changes

--- main.py
+++ main.py
@@ -5,31 +5,37 @@ import dedupe
 file = open("text.txt", "r")
 binaryFile = open("binary.txt", "wt+")
 # Path: text.txt
-i=0
+i=0 
 with open("text.txt", "r") as f:
     for line in f:
         i+=1
         # print("{} : {}".format(line.strip(),int(str( bin(int(binascii.hexlify(line.strip().encode('utf8')), 16)))[2:])))
-        binaryFile.write("{}\n".format(int(str( bin(int(binascii.hexlify(line.strip().encode('utf8')), 16)))[2:])))
+        binaryFile.write("{}\n".format(int(str(bin(int(binascii.hexlify(line.strip().encode('utf8')), 16)))[2:])))
 i=1
 binaryFile.close()
 binaryFile = open("binary.txt", "r")
 
 print("Now splitting binary file")
+chunk_size = 64
 with open ("binary.txt", "r") as myfile:
-    chunk = myfile.read(8)
+    chunk = myfile.read(chunk_size)
     while (chunk != ""):
         with open("Chunks/chunkFile_"+(str(i)), "wt+") as chunk_file:
-            chunk_file.write(chunk)
+            if('\n' in chunk):
+                chunk = chunk.split("\n")
+                chunk_file.write(chunk[0])
+                chunk_file.write(chunk[1])
+            else:
+                chunk=""
         i+=1
-        chunk = myfile.read(8)
+        chunk = myfile.read(chunk_size)
 
 
 # Using Hashing algorithms to find duplicates
 import hashlib
 
 print("Now finding duplicates")
-for j in range(1, i-1):
+for j in range(1, i):
     with open("Chunks/chunkFile_"+(str(j)), "r") as f:
         data = f.read()
         with open("Hashes/MDA5/hashFile_"+(str(j)), "wt+") as hash_file_MDA5:
@@ -39,7 +45,11 @@ for j in range(1, i-1):
 
 
 # Using Dedupe to find duplicates in MDA5 Hashes
-
+# deduper = dedupe.Dedupe(fields)
+# for j in range(1, i-1):
+#     with open("Hashes/MDA5/hashFile_"+(str(j)), "r") as f:
+#         data = f.read()
+#         deduper.sample(data)
 
 
 # Using Dedupe to find duplicates in SHA1 Hashes
