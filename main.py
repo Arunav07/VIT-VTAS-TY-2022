536e8438b8f4327c2271fe6fe3880bcf1d33dcb3
Arunav Chandra <39475778+Arunav07@users.noreply.github.com>
2022-12-15 01:55:40 +0530

    created a csv file of duplicates

--- main.py
+++ main.py
@@ -1,64 +1,62 @@
-import binascii
+import time
 
 file = open("text.txt", "r")
-binaryFile = open("binary.txt", "wt+")
 textFile = open("textWrite.txt", "wt+")
 # Path: text.txt
 i=0 
 with open("text.txt", "r", encoding='utf-8') as f:
     for line in f:
-        if(line=="\n"):
+        if(line=="\n" or line=="\r"):
             pass
         else:
-            binaryFile.write("{}\n".format(int(str(bin(int(binascii.hexlify(line.strip().encode('utf8')), 16)))[2:])))
             textFile.write("{}\n".format(line.strip()))
 i=1
-binaryFile.close()
-binaryFile = open("binary.txt", "r")
 
-print("Now splitting binary file")
-chunk_size = 10240
-with open ("binary.txt", "r") as myfile:
-    chunk = myfile.read(chunk_size)
+print("Now splitting into chunks")
+chunk_size = 1200
+with open ("textWrite.txt", "r") as myfile:
+    chunk = myfile.readline()
     while (chunk != ""):
-        with open("Chunks/chunkFile_"+(str(i)), "wt+") as chunk_file:
+        with open("Chunks/chunkFile_"+(str(i))+".txt", "wt+") as chunk_file:
             chunk_file.write(chunk)
         i+=1
-        chunk = myfile.read(chunk_size)
+        chunk = myfile.readline()
 
 
 # Using Hashing algorithms to find duplicates
 import hashlib
 
 print("Now finding duplicates")
+# time_MD5 = time.time()
+# time_SHA1 = time.time()
+time_SHA256 = time.time()
 for j in range(1, i):
-    with open("Chunks/chunkFile_"+(str(j)), "r") as f:
+    with open("Chunks/chunkFile_"+(str(j))+".txt", "r") as f:
         data = f.read()
-        with open("Hashes/MDA5/hashFile_"+(str(j)), "wt+") as hash_file_MDA5:
-            hash_file_MDA5.write(hashlib.md5(data.encode('utf-8')).hexdigest())
-        with open("Hashes/SHA1/hashFile_"+(str(j)), "wt+") as hash_file_SHA1:
-            hash_file_SHA1.write(hashlib.sha1(data.encode('utf-8')).hexdigest())
+
+        # with open("Hashes/MD5/hashFile_"+(str(j))+".txt", "wt+") as hash_file_MD5:
+        #     hash_file_MD5.write(hashlib.md5(data.encode('utf-8')).hexdigest())
+        # with open("Hashes/SHA1/hashFile_"+(str(j)), "wt+") as hash_file_SHA1:
+        #     hash_file_SHA1.write(hashlib.sha1(data.encode('utf-8')).hexdigest())
         with open("Hashes/SHA256/hashFile_"+(str(j)), "wt+") as hash_file_SHA256:
             hash_file_SHA256.write(hashlib.sha256(data.encode('utf-8')).hexdigest())
 
-dedupHashTable_MDA5 = {}
-for j in range(1, i):
-    with open("Hashes/MDA5/hashFile_"+(str(j)), "r") as f:
-        data = f.read()
-        if data in dedupHashTable_MDA5:
-            dedupHashTable_MDA5[data].append(j)
-        else:
-            dedupHashTable_MDA5[data] = [j]
-
-dedupHashTable_SHA1 = {}
-for j in range(1, i):
-    with open("Hashes/SHA1/hashFile_"+(str(j)), "r") as f:
-        data = f.read()
-        if data in dedupHashTable_SHA1:
-            dedupHashTable_SHA1[data].append(j)
-        else:
-            dedupHashTable_SHA1[data] = [j]
-
+# dedupHashTable_MD5 = {}
+# for j in range(1, i):
+#     with open("Hashes/MD5/hashFile_"+(str(j))+".txt", "r") as f:
+#         data = f.read()
+#         if data in dedupHashTable_MD5:
+#             dedupHashTable_MD5[data].append(j)
+#         else:
+#             dedupHashTable_MD5[data] = [j]
+# dedupHashTable_SHA1 = {}
+# for j in range(1, i):
+#     with open("Hashes/SHA1/hashFile_"+(str(j)), "r") as f:
+#         data = f.read()
+#         if data in dedupHashTable_SHA1:
+#             dedupHashTable_SHA1[data].append(j)
+#         else:
+#             dedupHashTable_SHA1[data] = [j]
 dedupHashTable_SHA256 = {}
 for j in range(1, i):
     with open("Hashes/SHA256/hashFile_"+(str(j)), "r") as f:
@@ -67,24 +65,38 @@ for j in range(1, i):
             dedupHashTable_SHA256[data].append(j)
         else:
             dedupHashTable_SHA256[data] = [j]
+# print("Duplicates found using MD5: ")
+# for k in range(len(dedupHashTable_MD5)):
+#     if(len(dedupHashTable_MD5[list(dedupHashTable_MD5.keys())[k]])>1):
+#     print( list(dedupHashTable_MD5.keys())[k], " : ", dedupHashTable_MD5[list(dedupHashTable_MD5.keys())[k]])
+# end_MD5 = time.time()
 
-print("Duplicates found using MDA5: ")
-for k in range(len(dedupHashTable_MDA5)):
-    if(len(dedupHashTable_MDA5[list(dedupHashTable_MDA5.keys())[k]])>1):
-        print("Duplicate files are: ",k)
-        print( list(dedupHashTable_MDA5.keys())[k], " : ", dedupHashTable_MDA5[list(dedupHashTable_MDA5.keys())[k]])
-        print(open(textFile.name).read().splitlines()[dedupHashTable_MDA5[list(dedupHashTable_MDA5.keys())[k]][0]-1])
 
-print("Duplicates found using SHA1: ")
-for k in range(len(dedupHashTable_SHA1)):
-    if(len(dedupHashTable_SHA1[list(dedupHashTable_SHA1.keys())[k]])>1):
-        print("Duplicate files are: ",k)
-        print( list(dedupHashTable_SHA1.keys())[k], " : ", dedupHashTable_SHA1[list(dedupHashTable_SHA1.keys())[k]])
-        print(open(textFile.name).read().splitlines()[dedupHashTable_SHA1[list(dedupHashTable_SHA1.keys())[k]][0]-1])
-
+# #converting 
+# print("Duplicates found using SHA1: ")
+# for k in range(len(dedupHashTable_SHA1)):
+#     if(len(dedupHashTable_SHA1[list(dedupHashTable_SHA1.keys())[k]])>1):
+#         print( list(dedupHashTable_SHA1.keys())[k], " : ", dedupHashTable_SHA1[list(dedupHashTable_SHA1.keys())[k]])
+# end_SHA1 = time.time()
 print("Duplicates found using SHA256: ")
 for k in range(len(dedupHashTable_SHA256)):
     if(len(dedupHashTable_SHA256[list(dedupHashTable_SHA256.keys())[k]])>1):
-        print("Duplicate files are: ",k)
-        print( list(dedupHashTable_SHA256.keys())[k], " : ", dedupHashTable_SHA256[list(dedupHashTable_SHA256.keys())[k]])
-        print(open(textFile.name).read().splitlines()[dedupHashTable_SHA256[list(dedupHashTable_SHA256.keys())[k]][0]-1])
+        # writing duplicates to text
+        locations = list(map(str, dedupHashTable_SHA256[list(dedupHashTable_SHA256.keys())[k]]))
+        print(locations)
+        with open("Duplicates.txt", "at+") as f:
+            f.write(open("Chunks/chunkFile_"+(str(dedupHashTable_SHA256[list(dedupHashTable_SHA256.keys())[k]][0]))+".txt", "r").read()[:-2]+","+("--").join(locations)+"\n")
+f.close()
+
+# converting text to csv
+import pandas as pd
+df = pd.read_csv("Duplicates.txt", sep=",", header=None)
+df.to_csv("Duplicates.csv", index=False)
+
+
+# f.close()
+end_SHA256 = time.time()
+
+# print("Time taken for MD5: ", end_MD5-time_MD5)
+# print("Time taken for SHA1: ", end_SHA1-time_SHA1)
+print("Time taken for SHA256: ", end_SHA256-time_SHA256)
