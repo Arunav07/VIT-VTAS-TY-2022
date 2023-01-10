23af982148d9566ef3e8d1944c77d01d8808796c
Sukhpreet Singh Bhatti <sukhpreetsingh06072001@gmail.com>
2023-01-10 10:40:27 +0530

    Multiple files upload partial complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,35 @@
 import React, { useState } from 'react';
 
 function App() {
-  const [file, setFile] = useState(null);
+  const [fileList, setFileList] = useState(null);
 
   const handleFileChange = (event) => {
-    setFile(event.target.files[0]);
+    setFileList(event.target.files);
   }
 
   const handleUpload = (event) => {
-    event.preventDefault();
+    // event.preventDefault();
+    if (!fileList) {
+      return;
+    }
 
     const formData = new FormData();
-    formData.append('file', file);
+    files.forEach((file, i) => {
+      formData.append(`file-${i}`, file, file.name);
+    });
+    // formData.append('file', file);
+    console.log(formData);
+    localStorage.setItem("Formdata",JSON.stringify(formData));
 
-    fetch('/server/upload', {
+
+    fetch('https://httpbin.org/post', {
       method: 'POST',
       body: formData
     })
     .then(response => response.json())
     .then(data => {
       try {
-        JSON.parse(data);
+        // JSON.parse(data);
         console.log('Success:', data);
     }
     catch (error) {
@@ -32,17 +41,28 @@ function App() {
     });
   }
   
+  const files = fileList ? [...fileList] : [];
+  
   return (
     <div className="App">
       <h2>File Upload</h2>
       <form>
         <span className="uploadContainer">
           <label htmlFor="file">Choose a file: </label>
-          <input type="file" id="file" onChange={handleFileChange}/>
+          <input type="file" id="file" onChange={handleFileChange} multiple/>
         </span>
+
+        <ul>
+        {files.map((file, i) => (
+          <li key={i}>
+            {file.name} - {file.type}
+          </li>
+        ))}
+      </ul>
+
         <button className="uploadButton" type='submit' onClick={(e)=>{
           handleUpload(e);
-          console.log(file)
+          console.log(files)
         }} >Upload</button>
       </form>
     </div>
