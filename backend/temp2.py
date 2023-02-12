443162801eb5e4ac8cb1cdb9cdc5fc68b6a486a9
Sukhpreet Singh Bhatti <sukhpreetsingh06072001@gmail.com>
2023-02-12 13:19:10 +0530

    Individual file shrinking is done

--- backend/temp2.py
+++ backend/temp2.py
@@ -31,51 +31,59 @@ def upload():
     files = request.files.getlist('file')
     global numberofFiles, AllFiles
     numberofFiles = len(files)
-    print("Files uploaded : ",files)
-    print("Number of files uploaded : ",numberofFiles)
+    # print("Files uploaded : ",files)
+    # print("Number of files uploaded : ",numberofFiles)
     for file in files:
         # print(file.filename)
         #Unique_chunks is a dictionary that stores the hash value as key and the chunk content as value . It has the unique hashed value of chunks and its content
         Unique_chunks = {}
         #Duplic is a dictionary that stores the hash value as key and the number of times it occurs as value
         Duplic = {}
-        # total_size = 0
-        hashList = []
         #It is a list of all hash value calculated
+        hashList = []
         # hashedSet = set()
-        fileR = bytes(file.read())
         # print(fileR)
-        chunk_size = 1024
+        chunk_size = 600
         counter = 0
-        OriginalSize = 0     
+        ShrinkSize = 0
+        OriginalSize = 0 
+
+        fileR = bytes(file.read())
+        
         for i in range(0, len(fileR), chunk_size):
             counter += 1
-            OriginalSize += len(fileR[i:i + chunk_size])
             chunk = str(bytes(fileR[i:i + chunk_size]),'utf-8')
+            OriginalSize += len(fileR[i:i + chunk_size])
             hashedValue = hashlib.md5(fileR[i:i + chunk_size]).hexdigest()
             hashList.append(hashedValue)
             # hashedSet.add(hashedValue)
+
+            if Unique_chunks.get(hashedValue) is None:
+                ShrinkSize += len(fileR[i:i + chunk_size])
+
             Unique_chunks[hashedValue] = chunk
         
         for hash in hashList:
-            Duplic[hash] =hashList.count(hash)
+            Duplic[hash] = hashList.count(hash)
         
-        print (hashList)
+        # print (hashList)
         # print (hashedSet)
-        print (len(Unique_chunks))
-        print (Duplic)
+        # print (len(Unique_chunks))
+        # print (Duplic)
+        # print ("Original Size : ",OriginalSize)
+        # print ("Shrinkage Size :",ShrinkSize)
 
-        checkFileDuplicate(Unique_chunks,file.filename)
-        ResultData = createFile(Duplic, Unique_chunks,file.filename) # Data = dictionary()
-        AllFiles[file.filename] = [ResultData, OriginalSize]
+        createChunks(Unique_chunks,file.filename)
+        createShrinkFile(Unique_chunks,file.filename) # Data = dictionary()
+        AllFiles[file.filename] = [OriginalSize,ShrinkSize]
 
     
-    print("AllFiles",AllFiles)
+    # print("AllFiles",AllFiles)
     
     return make_response(jsonify({'message': 'Success', "AllFiles": AllFiles, 'numberofFiles': numberofFiles}), 200)
 
 
-def checkFileDuplicate(Unique_chunks,filename):        
+def createChunks(Unique_chunks,filename):        
     
     i=0
     os.makedirs('backend/Chunks/Chunks-'+filename, exist_ok=True)
@@ -85,12 +93,11 @@ def checkFileDuplicate(Unique_chunks,filename):
         i+=1
 
 
-def createFile(Duplic, Unique_chunks,filename):
-    f = open("C:\\Users\\gurve\\My Projects\\Data_Deduplication-VIT-Veritas-\\db\\test.txt","wt+")
+def createShrinkFile(Unique_chunks,filename):
+    f = open("C:\\Users\\gurve\\My Projects\\Data_Deduplication-VIT-Veritas-\\backend\\Chunks\\Chunks-"+filename+"\\"+filename,"wt+")
     for hash_value, chunk in Unique_chunks.items():  
         f.write(chunk)
     f.close()
-    return jsonify({'message': 'Success','ShrinkSize': os.stat("C:\\Users\\gurve\\My Projects\\Data_Deduplication-VIT-Veritas-\\backend\\Chunks\\Chunks-"+filename).st_size, 'Duplic': Duplic, 'UniqueChunks': Unique_chunks})
 
 if __name__ == '__main__':
     app.run(debug=True)
